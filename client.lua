511e6cc541257b8f90ba6af6571fdfc0249f7303
Marty <117897481+Marty519@users.noreply.github.com>
2023-02-24 17:43:41 -0600

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,8 @@
+-- Made by Marty.#0519
+-- Join my Discord! https://discord.gg/XBHk4ASgvh
+
+AddEventHandler('onClientGameTypeStart', function()
+    TriggerEvent('chat:addMessage', {
+        args = { '~b~Welcome to Your Server Name Here! If you have any Questions, please join our Discord! ~r~discord.gg/YOURINVITEHERE' }
+        })
+    end)
---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version "cerulean"
+game "gta5"
+
+author 'Marty.#3192'
+description 'A simple chat Welcome Script'
+version '1.0.0'
+
+client_script "client.lua"
